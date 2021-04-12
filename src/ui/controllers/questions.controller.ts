59252ed4abab0a920ecbf910bb8e59ac49d5929d
Jardesson Ellíudo <42861820+jardesson@users.noreply.github.com>
2021-04-11 21:27:16 -0300

    Ajustes no Controller e adição do model

--- src/ui/controllers/questions.controller.ts
+++ src/ui/controllers/questions.controller.ts
@@ -14,40 +14,40 @@ export class QuestionsController {
      */
     @Post(':question_id/answer')
     public async saveAnswer(req: Request, res: Response): Promise<Response> {
-        return res.status(HttpStatus.CREATED).send('Resposta salva')
+        return res.status(HttpStatus.CREATED).send('Save Answer to the Question')
     }
     
     /**
-     * 
+     * Get question by id
      * @param req 
      * @param res 
      * @returns 
      */
     @Get(':question_id')
     public async getQuestionsById(req: Request, res: Response): Promise<Response> {
-        return res.status(HttpStatus.OK).send('Questão por id')
+        return res.status(HttpStatus.OK).send('Get question by id')
     }
 
     /**
-     * 
+     * Get answer by question id
      * @param req 
      * @param res 
      * @returns 
      */
     @Get(':question_id/answer')
-    public async getAnswerQuestionById(req: Request, res: Response): Promise<Response> {
-        return res.status(HttpStatus.OK).send('Respostas da questão por id')
+    public async getAnswerByQuestionId(req: Request, res: Response): Promise<Response> {
+        return res.status(HttpStatus.OK).send('Get answer by question id')
     }
 
     /**
-     * 
+     * Delete the question
      * @param req 
      * @param res 
      * @returns 
      */
     @Delete(':question_id')
     public async deleteQuestionById(req: Request, res: Response): Promise<Response> {
-        return res.status(HttpStatus.OK).send('Questão deletada')
+        return res.status(HttpStatus.NO_CONTENT).send()
     }
 
 
